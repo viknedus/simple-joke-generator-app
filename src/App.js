6f946939c76bd05db4136d9e1a3bd772dc14c0bc
viknedus <eduvanvictor@yahoo.com>
2021-11-01 09:41:00 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
+
+  // custom hook
+  function useRandomJoke(firstName, lastName) {
+    const [joke, setJoke] = useState("");
+
+    useEffect(() => {
+      const fetchJoke = async () =>
+        await fetch(
+          `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setJoke(data.value.joke);
+          });
+
+      fetchJoke();
+    }, [firstName, lastName]);
+    return joke;
+  }
+
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Joke Generator</h1>
+        <p>{joke}</p>
+        <button onClick={() => window.location.reload(false)}>Click to generate new joke!</button>
+
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate new Joke with your name in it, maybe</button>
+        </form>
       </header>
     </div>
   );
